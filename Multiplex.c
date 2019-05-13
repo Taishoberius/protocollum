0c90189a34461ffd7c651d86e13930384b9b9160
Cédric Nanteau <cedric.nanteau@gmail.com>
2019-05-13 18:21:35 +0200

    ✏️ better intent

--- Multiplex.c
+++ Multiplex.c
@@ -3,5 +3,5 @@
 #include "server.h"
 
 int main(int argc, char const *argv[]) {
-  handle_server_events(init_server(AF_INET, INADDR_ANY, 7777, SOCK_STREAM, 0));
+    handle_server_events(init_server(AF_INET, INADDR_ANY, 7777, SOCK_STREAM, 0));
 }
